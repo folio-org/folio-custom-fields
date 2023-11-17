89de05c12799c16c79e33e2997f85e07f3f6f46c
Gurleen Kaur <34331959+gurleenkaurbp@users.noreply.github.com>
2023-11-17 14:40:53 +0530

    Revert "[FCFIELDS-43] - For Help Text field relax the validation when it is null. (#39)"
    This reverts commit d796d2c71584adbbfde65ca646f272fd38b22d34.

--- src/test/java/org/folio/validate/definition/HelpTextValidatorTest.java
+++ src/test/java/org/folio/validate/definition/HelpTextValidatorTest.java
@@ -3,10 +3,6 @@ package org.folio.validate.definition;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import org.folio.rest.jaxrs.model.CustomField;
-import org.folio.spring.TestConfiguration;
-import org.folio.test.util.TestUtil;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,6 +10,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import org.folio.rest.jaxrs.model.CustomField;
+import org.folio.spring.TestConfiguration;
+import org.folio.test.util.TestUtil;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
@@ -30,14 +30,6 @@ public class HelpTextValidatorTest {
     expectedEx.expectMessage("The 'helpText' length cannot be more than 100");
     final CustomField customField = TestUtil.readJsonFile(
       "fields/post/postCustomFieldHelpTextInvalid.json", CustomField.class);
-    validator.validateDefinition(customField);
-  }
-
-  @Test
-  public void shouldPassValidationForHelpTextWithNull() throws IOException, URISyntaxException {
-    Assert.assertFalse(expectedEx.isAnyExceptionExpected());
-    final CustomField customField = TestUtil.readJsonFile(
-      "fields/post/postCustomFieldHelpTextNull.json", CustomField.class);
     validator.validateDefinition(customField);
   }
 }
