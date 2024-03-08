e526bd0d4a2ed961e6b12f18ac0ce5c8e2cfe31b
Gurleen Kaur <gurleen_kaur1@epam.com>
2024-03-08 13:03:26 +0530

    [FCFIELDS-51] - Upgrade rmb and vertex version

--- src/test/java/org/folio/validate/CustomFieldValue.java
+++ src/test/java/org/folio/validate/CustomFieldValue.java
@@ -3,12 +3,14 @@ package org.folio.validate;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.validation.Valid;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.Valid;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CustomFieldValue {
   @JsonIgnore
